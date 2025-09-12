e8cb740858ac113d1e788aa90acb52ee1e14f11f
puneethkumar01 <puneethkumar1405@gmail.com>
2025-09-12 07:07:49 +0530

    deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api", // Backend URL
+  baseURL: "https://busbuddy-backend-c6dg.onrender.com/api",
+  //"http://localhost:4000/api", // Backend URL
 });
 
 export default API;
